1eec8ccd110157c79dc346a18a9977fd2a5e3033
Oyunbat <oyunbat9958@gmail.com>
2025-12-07 22:15:04 +0800

    build error fixed

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,19 @@ import HeroSection from "./component/HeroSection";
 import AboutSection from "./component/AboutSection";
 import ServiceSection from "./component/ServiceSection";
 import ContactSection from "./component/ContactSection";
+import { useEffect } from "react";
 import TeamSection from "./component/TeamSection";
-import Footer from "./component/Footer";
 export default function Home() {
 
-  const lenis = new Lenis();
-  function raf(time: number) {
-    lenis.raf(time);
+  useEffect(() => {
+
+    const lenis = new Lenis();
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
     requestAnimationFrame(raf);
-  }
-  requestAnimationFrame(raf);
+  }, []);
 
   return (
     <div className="font-sans text-gray-800 antialiased bg-white">
